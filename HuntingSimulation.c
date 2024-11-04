52a11b00d0baca21405936ae520f0c1aa509f301
feifly99 <148175317+feifly99@users.noreply.github.com>
2024-11-04 21:01:02 +0800

    Update HuntingSimulation.c

--- HuntingSimulation.c
+++ HuntingSimulation.c
@@ -15,11 +15,13 @@ void initializeTargetUSV(OUTPTR PTUSV* Tusv, IN Point position, IN double normal
 	(*Tusv)->headDirection = headDirection;
 	return;
 }
-void initializeHunterUsv(OUTPTR PHUSV* Husv, IN Point position, IN double velocity, IN double movableAngleRange, IN double headDirection)
+void initializeHunterUsv(OUTPTR PHUSV* Husv, IN Point position, IN double velocity, IN double movableAngleRange, IN double headDirection, IN size_t originalIndex)
 {
 	*Husv = (PHUSV)malloc(sizeof(HUSV));
 	(*Husv)->pos.x = position.x;
 	(*Husv)->pos.y = position.y;
+	(*Husv)->originalIndex = originalIndex;
+	(*Husv)->currentIndex = 0;
 	(*Husv)->velocity = velocity;
 	(*Husv)->movableAngleRange = movableAngleRange;
 	(*Husv)->headDirection = headDirection;
@@ -32,7 +34,7 @@ void initializeHunterUsv(OUTPTR PHUSV* Husv, IN Point position, IN double veloci
 	(*Husv)->hunterListEntry.Blink = NULL;
 	return;
 }
-void buildCycleList(IN_OUT PHUSV* hunter)
+void buildCycleListByHuntersOriginalIndex(IN_OUT PHUSV* hunter)
 {
 	for (size_t j = 0; j < huntersNum - 1; j++)
 	{
@@ -123,6 +125,21 @@ double closeBetter(IN double indicator, IN double numberCloseTo)
 {
 	const double epsilon = 1e-6;
 	return 1.0 / (1.0 + mabs(indicator - numberCloseTo) + epsilon);
+}
+void sortArray(IN double* array, IN size_t size)
+{
+	for (size_t i = 0; i < size - 1; i++)
+	{
+		for (size_t j = 0; j < size - 1 - i; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				double temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+			}
+		}
+	}
 }
 BOOL floatSame10(IN double x, IN double y)
 {
@@ -317,8 +334,29 @@ void movingNormalTargetRandomly(OUT PTUSV* target)
 	int flag = rand() % 2;
 	int angle_x = (flag == 0) ? (rand() % 70 - rand() % 24) : (-1) * (rand() % 70 - rand() % 24);
 	int angle_y = (flag == 0) ? (rand() % 110 - rand() % 14) : (-1) * (rand() % 110 - rand() % 14);
-	(*target)->pos.x += RtN * flag * cosa((double)angle_x);
-	(*target)->pos.y += RtN * flag * sina((double)angle_y);
+	if(flag == 0)
+	{
+		(*target)->pos.x += RtN * (-1) * cosa((double)angle_x);
+		(*target)->pos.y += RtN * (-1) * sina((double)angle_y);
+	}
+	else
+	{
+		(*target)->pos.x += RtN * cosa((double)angle_x);
+		(*target)->pos.y += RtN * sina((double)angle_y);
+	}
+	/*(*target)->pos.x += RtN * cosa((double)240.0);
+	(*target)->pos.y += RtN * sina((double)240.0);*/
+	return;
+}
+void movingNormalTargetStably(OUT PTUSV* target)
+{
+	double angle1 = 60.0;
+	double angle2 = 120.0;
+	double angle3 = 240.0;
+	double angle4 = 330.0;
+	double angle = angle1;
+	(*target)->pos.x += RtN * cosa((double)angle);
+	(*target)->pos.y += RtN * sina((double)angle);
 	return;
 }
 //Geometry Functions:
@@ -372,18 +410,18 @@ BOOL checkPointIsSafe(IN Point loc, IN PTUSV target)
 	return calculate_V_model(&temp) - RtN > target->safetyRadius + RtN + Rh;
 }
 //Surroundding Simulation Functions:
-void _SURROUNDING_getHuntersFourEIByHuntersLocAndHeadDirection(IN PHUSV hunter, IN PTUSV target)
+void _SURROUNDING_getHuntersFourEIByHuntersLocAndHeadDirection(IN_OUT PHUSV* hunter, IN PTUSV target)
 {
 	//mostSafetyRadius是目标的安全圈半径加上一个采样时间内目标能够移动的长度；
 	//RtN和Rh的系数必须大于等于1才能确保最糟糕的情况下（捕食者和目标相向而行）目标也不会发现捕食者.
 	double mostSafetyRadius = target->safetyRadius + 1 * RtN + 1 * Rh;
-	PHUSV curr = hunter;
+	PHUSV curr = *hunter;
 	PHUSV next = CONTAINING_RECORD(curr->hunterListEntry.Flink, HUSV, hunterListEntry);
 	PHUSV prev = CONTAINING_RECORD(curr->hunterListEntry.Blink, HUSV, hunterListEntry);
-	hunter->evaluationIndicator.nearAngleDiffer = calculate_P$P$P_unsignedAngle(curr->pos, target->pos, next->pos) - calculate_P$P$P_unsignedAngle(prev->pos, target->pos, curr->pos);
-	hunter->evaluationIndicator.headDirectionDiffer = mabs(curr->headDirection - target->headDirection);
-	hunter->evaluationIndicator.idealAngleDiffer = mabs(calculate_P$P$P_unsignedAngle(curr->pos, target->pos, next->pos)) - ((double)360 / (double)huntersNum);
-	hunter->evaluationIndicator.idealDistanceDiffer = calculate_P$P_distance(curr->pos, target->pos) - mostSafetyRadius;
+	(*hunter)->evaluationIndicator.nearAngleDiffer = calculate_P$P$P_unsignedAngle(curr->pos, target->pos, next->pos) - calculate_P$P$P_unsignedAngle(prev->pos, target->pos, curr->pos);
+	(*hunter)->evaluationIndicator.headDirectionDiffer = mabs(curr->headDirection - target->headDirection);
+	(*hunter)->evaluationIndicator.idealAngleDiffer = mabs(calculate_P$P$P_unsignedAngle(curr->pos, target->pos, next->pos)) - ((double)360 / (double)huntersNum);
+	(*hunter)->evaluationIndicator.idealDistanceDiffer = calculate_P$P_distance(curr->pos, target->pos) - mostSafetyRadius;
 	return;
 }
 double _SURROUNDING_getRewardPointsForHunterByHuntersEI(IN PHUSV hunter)
@@ -395,14 +433,14 @@ double _SURROUNDING_getRewardPointsForHunterByHuntersEI(IN PHUSV hunter)
 
 	double J = 0.0;
 	//误差线episilon_angle:以目标为中心的合围图形中心角越接近【360 / n + episilon_angle】越好
-	double episilon_angle = 2.0;
+	double episilon_angle = 3.0;
 	//误差线episilon，捕食者和目标最大安全圈的距离（mostSafetyRadius）越接近0.1越好；
 	//注：这里只能接受mostSafetyRadius + 0.1, 如果捕食者进入了最大安全圈内部直接拒绝进入.
-	double episilon_distance = 0.2;
+	double episilon_distance = 0.8;
 
 	if (hunter->evaluationIndicator.idealDistanceDiffer <= 0)
 	{
-		J = -10000.0;
+		J = -100000.0;
 	}
 	else
 	{
@@ -413,66 +451,144 @@ double _SURROUNDING_getRewardPointsForHunterByHuntersEI(IN PHUSV hunter)
 }
 void _SURROUNDING_changingHunterInfomationByRewardFunction(IN_OUT PHUSV* hunter, IN PTUSV target)
 {
+	double currentHunterValidRegionAngle = calculate_P$P$P_unsignedAngle(CONTAINING_RECORD((*hunter)->hunterListEntry.Blink, HUSV, hunterListEntry)->pos, target->pos, CONTAINING_RECORD((*hunter)->hunterListEntry.Flink, HUSV, hunterListEntry)->pos);
 	Point nextHunterIdealLoc = { 0 };
 	PHUSV nextHunterIdealInfo = NULL;
-
 	nextHunterIdealInfo = (PHUSV)malloc(sizeof(HUSV));
 	memcpy(nextHunterIdealInfo, *hunter, sizeof(HUSV));
-
 	double newMaxRewardPoints = -9999.0;
-
-	int global_j = 0;
-	int global_i = 0;
-
+	typedef struct _globalIndex
+	{
+		int i;
+		int j;
+	}GI, *PGI;
+	typedef struct _globalInfoStack
+	{
+		GI* globalIndex;
+		PGI stackTopPointer;
+		PGI stackButtomPointer;
+	}GIS, *PGIS;
+	PGIS stack = (PGIS)malloc(sizeof(GIS));
+	stack->globalIndex = (GI*)malloc(STACK_MAX_SIZE * sizeof(GI));
+	stack->stackTopPointer = stack->globalIndex;
+	stack->stackButtomPointer = stack->globalIndex;
 	for (int j = 0; j < 180; j++)
 	{
-		for (int i = 1; i <= 20; i++)
+		for (int i = 0; i <= 20; i++) //注意i从0开始，可能保持不动
 		{
 			nextHunterIdealInfo->pos.x = (*hunter)->pos.x + (i * ((double)huntersVelocity / (double)20.0)) * cosa(j * 2);
 			nextHunterIdealInfo->pos.y = (*hunter)->pos.y + (i * ((double)huntersVelocity / (double)20.0)) * sina(j * 2);
-			_SURROUNDING_getHuntersFourEIByHuntersLocAndHeadDirection(nextHunterIdealInfo, target);
-			//必须先得到EI才能进行评分，所以要有getHuntersFourEIByHuntersLocAndHeadDirection
-			//<<<<<此循环不会对*hunter造成副作用>>>>>
+			_SURROUNDING_getHuntersFourEIByHuntersLocAndHeadDirection(&nextHunterIdealInfo, target);
 			if (_SURROUNDING_getRewardPointsForHunterByHuntersEI(nextHunterIdealInfo) > newMaxRewardPoints)
 			{
-				//newMaxRewardPoints是最大值判定变量，每当有新的最大值就更新此newMaxRewardPoints.
 				newMaxRewardPoints = _SURROUNDING_getRewardPointsForHunterByHuntersEI(nextHunterIdealInfo);
-				//此循环的最终目的只是输出global_ij来确定*hunter的下一个最优位置.
-				global_j = j;
-				global_i = i;
+				stack->stackTopPointer->i = i;
+				stack->stackTopPointer->j = j;
+				stack->stackTopPointer++;
 			}
 		}
 	}
-
-	nextHunterIdealLoc.x = (*hunter)->pos.x + (global_i * ((double)huntersVelocity / (double)20.0)) * cosa(global_j * 2);
-	nextHunterIdealLoc.y = (*hunter)->pos.y + (global_i * ((double)huntersVelocity / (double)20.0)) * sina(global_j * 2);
-
-	double dis[2] = { 0.0 };
-	double huntersSafetyNeighborDistance = 1.2 * Rh;
-	
-	nextHunterIdealInfo->pos.x = nextHunterIdealLoc.x;
-	nextHunterIdealInfo->pos.y = nextHunterIdealLoc.y;
-
-	//分布式控制：只需要最近的两个船的信息来确保安全性；
-	//如果下一次移动的理想位置会导致两个捕食者距离过近，那么就让这个船停滞不动。
-
-	dis[0] = calculate_P$P_distance(CONTAINING_RECORD(nextHunterIdealInfo->hunterListEntry.Flink, HUSV, hunterListEntry)->pos, nextHunterIdealInfo->pos);
-	dis[1] = calculate_P$P_distance(CONTAINING_RECORD(nextHunterIdealInfo->hunterListEntry.Blink, HUSV, hunterListEntry)->pos, nextHunterIdealInfo->pos);
-	
-	if (dis[0] <= huntersSafetyNeighborDistance || dis[1] <= huntersSafetyNeighborDistance)
+	double neighborDistanceDifferOfNextLoc[2] = { 0.0 };
+	double huntersSafetyNeighborDistance = 1.1 * Rh;
+	stack->stackTopPointer--;
+	while(stack->stackTopPointer != stack->stackButtomPointer)
 	{
-		free(nextHunterIdealInfo);
-		nextHunterIdealInfo = NULL;
-		return;
+		nextHunterIdealLoc.x = (*hunter)->pos.x + ((double)(stack->stackTopPointer->i) * ((double)huntersVelocity / (double)20.0)) * cosa((double)(stack->stackTopPointer->j * 2.0));
+		nextHunterIdealLoc.y = (*hunter)->pos.y + ((double)(stack->stackTopPointer->i) * ((double)huntersVelocity / (double)20.0)) * sina((double)(stack->stackTopPointer->j * 2.0));
+		nextHunterIdealInfo->pos.x = nextHunterIdealLoc.x;
+		nextHunterIdealInfo->pos.y = nextHunterIdealLoc.y;
+		neighborDistanceDifferOfNextLoc[0] = calculate_P$P_distance(CONTAINING_RECORD(nextHunterIdealInfo->hunterListEntry.Flink, HUSV, hunterListEntry)->pos, nextHunterIdealInfo->pos);
+		neighborDistanceDifferOfNextLoc[1] = calculate_P$P_distance(CONTAINING_RECORD(nextHunterIdealInfo->hunterListEntry.Blink, HUSV, hunterListEntry)->pos, nextHunterIdealInfo->pos);
+		if 
+		(
+			(neighborDistanceDifferOfNextLoc[0] <= huntersSafetyNeighborDistance || neighborDistanceDifferOfNextLoc[1] <= huntersSafetyNeighborDistance)
+			||
+			(
+				calculate_P$P$P_unsignedAngle(CONTAINING_RECORD((*hunter)->hunterListEntry.Blink, HUSV, hunterListEntry)->pos, target->pos, nextHunterIdealInfo->pos)
+				>=
+				currentHunterValidRegionAngle
+			)
+			||
+			(
+				calculate_P$P$P_unsignedAngle(nextHunterIdealInfo->pos, target->pos, CONTAINING_RECORD((*hunter)->hunterListEntry.Flink, HUSV, hunterListEntry)->pos)
+				>=
+				currentHunterValidRegionAngle
+			)
+		)
+		{
+			stack->stackTopPointer--;
+			if (stack->stackTopPointer == stack->stackButtomPointer)
+			{
+				printf("警告：最优全局i_j已经到达栈底.\n");
+				nextHunterIdealLoc.x = (*hunter)->pos.x;
+				nextHunterIdealLoc.y = (*hunter)->pos.y;
+				break;
+			}
+			return;
+		}
+		else
+		{
+			break;
+		}
 	}
-	//获得了最优global_ij后，先更新*hunter的位置，再根据*hunter新的位置去更新*hunter的EI；
-	//对输入参数*hunter的副作用仅在此处实现；
-	//根据获得的最优global_ij来转换成新的*hunter最优坐标，依新的最优坐标来更新*hunter的EI.
-	(*hunter)->pos.x = nextHunterIdealLoc.x;
-	(*hunter)->pos.y = nextHunterIdealLoc.y;
-	_SURROUNDING_getHuntersFourEIByHuntersLocAndHeadDirection(*hunter, target);
+	updateHunterPosByNewPoint(hunter, nextHunterIdealLoc);
+	_SURROUNDING_getHuntersFourEIByHuntersLocAndHeadDirection(hunter, target);
+	free(stack->globalIndex);
+	stack->globalIndex = NULL;
+	free(stack);
+	stack = NULL;
 	free(nextHunterIdealInfo);
 	nextHunterIdealInfo = NULL;
+	return;
+}
+void _SURROUNDING_remakeHuntersCycleListByRelativeLocCompareToTarget(IN_OUT PHUSV (*hunter)[huntersNum], IN PTUSV target)
+{
+	double relativeAngles[huntersNum] = { 0.0 };
+	V relativeVectors[huntersNum] = { {0, 0} };
+	size_t k[huntersNum] = { 0 };
+	fors(
+		huntersNum,
+		relativeVectors[j].a = ((*hunter)[j])->pos.x - target->pos.x;
+		relativeVectors[j].b = ((*hunter)[j])->pos.y - target->pos.y;
+	);
+	fors(
+		huntersNum,
+		relativeAngles[j] = calculate_OX$V_unsignedAngle(&relativeVectors[j]);
+	);
+	//debug
+	fors(
+		huntersNum,
+		ckFloatValue(relativeAngles[j]);
+	);
+	for (size_t j = 0; j < huntersNum - 1; j++)
+	{
+		for (size_t i = 0; i < huntersNum - 1 - j; i++)
+		{
+			if (relativeAngles[i] > relativeAngles[i + 1])
+			{
+				double temp = relativeAngles[i];
+				relativeAngles[i] = relativeAngles[i + 1];
+				relativeAngles[i + 1] = temp;
+			}
+		}
+	}
+	for (size_t j = 0; j < huntersNum; j++)
+	{
+		for (size_t i = 0; i < huntersNum; i++)
+		{
+			if (floatSame10(calculate_OX$V_unsignedAngle(&relativeVectors[i]), relativeAngles[j]))
+			{
+				k[j] = i;
+				break;
+			}
+		}
+	}
+	fors(
+		huntersNum,
+		((*hunter)[j])->currentIndex = k[j];
+	);
+	buildCycleListByHuntersOriginalIndex(&(*hunter)[0]);
+	//system("pause");
 	return;
 }
 BOOLEAN isSurroundingSuccess(IN PHUSV hunter[huntersNum], IN PTUSV target)
@@ -482,8 +598,9 @@ BOOLEAN isSurroundingSuccess(IN PHUSV hunter[huntersNum], IN PTUSV target)
 	//maxTolerance值必须大于_SURROUNDING_getRewardPointsForHunterByHuntersEI中定义的episilon_distance；
 	//否则永不收敛
 
-	double maxDistanceTolerance = 0.8;
-	double maxAngleTolerance = 2.5;
+	double maxAngleTolerance = 4.50;
+	double maxDistanceTolerance = 1.20;
+	
 
 	double d[huntersNum] = { 0 };
 	double a[huntersNum] = { 0 };
@@ -509,23 +626,39 @@ BOOLEAN isSurroundingSuccess(IN PHUSV hunter[huntersNum], IN PTUSV target)
 	
 }
 //Hunters Hunting Method Simulation Functions:
-void _HUNTER_goTowardsTargetDirectly(IN PHUSV hunter[huntersNum], IN PTUSV target)
+void _HUNTER_goTowardsTargetDirectly(IN_OUT PHUSV* hunter, IN PTUSV target)
 {
-	V direction[huntersNum] = { {0} };
-	fors(
-		huntersNum,
-		direction[j].a = target->pos.x - hunter[j]->pos.x;
-		direction[j].b = target->pos.y - hunter[j]->pos.y;
-	);
-	double angle[huntersNum] = { 0.0 };
-	fors(
-		huntersNum,
-		angle[j] = calculate_OX$V_unsignedAngle(&direction[j]);
-	);
-	fors(
-		huntersNum,
-		updateHunterPosByMovingDistanceAndUnsignedAngle(&hunter[j], Rh, angle[j]);
-	);
+	double neighborSafeyDistance = 1.2 * Rh;
+	Point currUsvPos = (*hunter)->pos;
+	Point prevUsvPos = (CONTAINING_RECORD((*hunter)->hunterListEntry.Blink, HUSV, hunterListEntry))->pos;
+	Point nextUsvPos = (CONTAINING_RECORD((*hunter)->hunterListEntry.Flink, HUSV, hunterListEntry))->pos;
+	V nextIdealDirection = { target->pos.x - currUsvPos.x, target->pos.y - currUsvPos.y };
+	double nextIdealMovingAngle = calculate_OX$V_unsignedAngle(&nextIdealDirection);
+	Point nextIdealPoint = { currUsvPos.x + Rh * cosa(nextIdealMovingAngle),currUsvPos.y + Rh * sina(nextIdealMovingAngle) };
+	if (calculate_P$P_distance(nextIdealPoint, prevUsvPos) >= neighborSafeyDistance && calculate_P$P_distance(nextIdealPoint, nextUsvPos) >= neighborSafeyDistance)
+	{
+		updateHunterPosByNewPoint(hunter, nextIdealPoint);
+	}
+	else
+	{
+		for (size_t j = 0; j < 360; j++)
+		{
+			for (size_t i = 1; i <= 20; i++)
+			{
+				nextIdealPoint.x = currUsvPos.x + i * ((double)(Rh) / (double)20.0) * cosa(nextIdealMovingAngle);
+				nextIdealPoint.y = currUsvPos.y + i * ((double)(Rh) / (double)20.0) * sina(nextIdealMovingAngle);
+				if (calculate_P$P_distance(nextIdealPoint, target->pos) >= calculate_P$P_distance(currUsvPos, target->pos) && calculate_P$P_distance(nextIdealPoint, prevUsvPos) >= neighborSafeyDistance && calculate_P$P_distance(nextIdealPoint, nextUsvPos) >= neighborSafeyDistance)
+				{
+					updateHunterPosByNewPoint(hunter, nextIdealPoint);
+					break;
+				}
+				else
+				{
+					continue;
+				}
+			}
+		}
+	}	
 	return;
 }
 //Target Escaping Simulation Functions:
@@ -613,12 +746,154 @@ void _TARGET_escapingAngleByVectorMethod(IN PHUSV hunter[huntersNum], IN PTUSV* 
 	//返回
 	return;
 }
+static double getRewardPointsForTargetByExtremeAngleDifferAndClosestHunterDistance(IN PHUSV hunter[huntersNum], IN PTUSV target)
+{
+	double J = 0.0;
+	double closestDistance = 0.0;
+	double extremeAngleDiffer = 0.0;
+	double distance[huntersNum] = { 0.0 };
+	V v[huntersNum] = { {0, 0} };
+	double angle[huntersNum] = { 0.0 };
+	double angle_differ[huntersNum] = { 0.0 };
+
+	fors(
+		huntersNum,
+		distance[j] = calculate_P$P_distance(hunter[j]->pos, target->pos);
+	);
+	sortArray(distance, huntersNum);
+	closestDistance = distance[0];
+
+	fors(
+		huntersNum,
+		v[j].a = hunter[j]->pos.x - target->pos.x;
+		v[j].b = hunter[j]->pos.y - target->pos.y;
+	);
+
+	fors(
+		huntersNum,
+		angle[j] = calculate_OX$V_unsignedAngle(&v[j]);
+	);
+	sortArray(angle, huntersNum);
+
+	fors(
+		huntersNum,
+		if (j == 0)
+		{
+			angle_differ[j] = 360 - (angle[huntersNum - 1] - angle[0]);
+		}
+		else
+		{
+			angle_differ[j] = angle[j - 1] - angle[j];
+		}
+	);
+	sortArray(angle_differ, huntersNum);
+	extremeAngleDiffer = angle_differ[huntersNum - 1] - angle_differ[0];
+
+	J = targetEscapingWeights_angle * closeBetter((1 / (extremeAngleDiffer + 0.00001)), 0.0) + targetEscapingWeights_distance * closeBetter((1 / (closestDistance + 0.00001)), 0.0);
+	
+	return J;
+}
 void _TARGET_escapingAngleByLearningMethod(IN PHUSV hunter[huntersNum], IN PTUSV* target)
 {
-	
+	PTUSV tempTarget = (PTUSV)malloc(sizeof(TUSV));
+	memcpy(tempTarget, *target, sizeof(TUSV));
+	double currPoints = -999999.0;
+	size_t global_i = 0;
+	size_t global_j = 0;
+	Point newPoint = { 0, 0 };
+
+	for (size_t j = 0; j < 360; j++)
+	{
+		for (size_t i = 1; i <= 20; i++)
+		{
+			tempTarget->pos.x = (*target)->pos.x + ((double)i) * ((double)RtE / 20.0) * cosa((double)j);
+			tempTarget->pos.y = (*target)->pos.y + ((double)i) * ((double)RtE / 20.0) * sina((double)j);
+			if (getRewardPointsForTargetByExtremeAngleDifferAndClosestHunterDistance(hunter, tempTarget) >= currPoints)
+			{
+				currPoints = getRewardPointsForTargetByExtremeAngleDifferAndClosestHunterDistance(hunter, tempTarget);
+				global_i = i;
+				global_j = j;
+			}
+		}
+	}
+
+	newPoint.x = (*target)->pos.x + ((double)global_i) * ((double)RtE / 30.0) * cosa((double)global_j);
+	newPoint.y = (*target)->pos.y + ((double)global_i) * ((double)RtE / 30.0) * sina((double)global_j);
+
+	updateTargetPosByNewPoint(target, newPoint);
+
+	free(tempTarget);
+	tempTarget = NULL;
+	return;
+}
+//Recycling Outcomes Judge Functions:
+BOOLEAN isRecyclingSuccess(IN PHUSV hunter[huntersNum], IN PTUSV target)
+{
+	double distance[huntersNum] = { 0.0 };
+	fors(
+		huntersNum,
+		distance[j] = calculate_P$P_distance(hunter[j]->pos, target->pos);
+	);
+	size_t idealRecyclingHuntersNum = 0x0;
+	fors(
+		huntersNum,
+		if (distance[j] <= huntersRecyclingRadius)
+		{
+			idealRecyclingHuntersNum++;
+		}
+	);
+	if (idealRecyclingHuntersNum >= (size_t)((huntersNum - 1) / 2) + 1)
+	{
+		return 1;
+	}
+	else
+	{
+		return 0;
+	}
+}
+BOOLEAN isTerminatingRecycling(IN PHUSV hunter[huntersNum], IN PTUSV target)
+{
+	double maxTerminatingToleranceLimit = 5.0;
+	static int remainTime = 8;
+	V O_Target = { target->pos.x, target->pos.y };
+	V v[huntersNum] = { {0, 0} };
+	fors(
+		huntersNum,
+		v[j].a = target->pos.x - hunter[j]->pos.x;
+		v[j].b = target->pos.y - hunter[j]->pos.y;
+	);
+	double angle[huntersNum] = { 0.0 };
+	fors(
+		huntersNum,
+		angle[j] = calculate_OX$V_unsignedAngle(&v[j]);
+	);
+	sortArray(angle, huntersNum);
+	fors(
+		huntersNum,
+		if (angle[j] - calculate_OX$V_unsignedAngle(&O_Target) > 10)
+		{
+			return 0;
+		}
+		else
+		{
+			continue;
+		}
+	);
+	fors(
+		remainTime--,
+		return 0;
+	);
+	if (angle[huntersNum - 1] - angle[0] >= maxTerminatingToleranceLimit)
+	{
+		return 1;
+	}
+	else
+	{
+		return 0;
+	}
 }
 //C-Python Interaction Functions:
-void makePythonFile(IN FILE* fp, IN PHUSV hunter[3], IN PTUSV target) 
+void makePythonFile(IN FILE* fp, IN PHUSV hunter[huntersNum], IN PTUSV target)
 {
 	Point h[huntersNum] = { {0} };
 
