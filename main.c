b574bceba1511055f35003e71dc30dc487bad49d
feifly99 <148175317+feifly99@users.noreply.github.com>
2025-04-09 19:14:37 +0800

    Update main.c

--- main.c
+++ main.c
@@ -5,6 +5,8 @@ static size_t huntersNumRealTime = (size_t)huntersNum;
 #define getFlink(j) CONTAINING_RECORD(hunter[j]->hunterListEntry.Flink, HUSV, hunterListEntry)
 #define getBlink(j) CONTAINING_RECORD(hunter[j]->hunterListEntry.Blink, HUSV, hunterListEntry)
 
+#pragma warning(disable: 6387 6011)
+
 int main()
 {
 	srand((UINT)time(NULL));
@@ -18,11 +20,11 @@ int main()
 	//一定一定要保证链表顺序！只要围捕的时候撞上绝对是链表顺序问题！！！
 	Point hunterPos[huntersNum] = { {0, 0} };
 	double beginAngle = 30.0;
-	double beginRadius = 4.0 * targetSafetyRadius;
+	double beginRadius = 3.5 * targetSafetyRadius;
 	for (size_t j = 0; j < huntersNum; j++)
 	{
-		hunterPos[j].x = targetPos.x + beginRadius * cosa(beginAngle + 20.0 * (double)j);
-		hunterPos[j].y = targetPos.y + beginRadius * sina(beginAngle + 20.0 * (double)j);
+		hunterPos[j].x = targetPos.x + beginRadius * cosa(beginAngle + 2.00 * (double)j);
+		hunterPos[j].y = targetPos.y + beginRadius * sina(beginAngle + 2.00 * (double)j);
 	}
 	initializeTargetUSV(
 		&target,
@@ -39,106 +41,74 @@ int main()
 			j
 		);
 	);
-	double obstacleSize = 6.0;
-	double obstacleVelocity = 0.85;
-	Point obstaclesCenterPos[obstaclesNum] =
-	{
-		{18.5, 15.2},
-		{12.3, -8.7},
-		{25.9, 20.1},
-		{9.6, 30.4},
-		{-5.2, 12.8},
-		{30.0, -10.5},
-		{15.8, -25.3},
-		{40.1, 18.7},
-		{8.9, 50.2},
-		{-20.4, -15.6}
-	};
-	double obstaclesMovingDirection[obstaclesNum] =
-	{
-		11.16,
-		212.16,
-		142.16,
-		122.16,
-		69.16,
-		192.16,
-		251.16,
-		144.16,
-		178.16,
-		288.16,
-	};
+	buildCycleListByHuntersOriginalIndex(hunter); //补全了hunter结构的LIST_ENTRY项
 
-	/*double obstacleSize = 0.0;
-	double obstacleVelocity = 0.00;
-	Point obstaclesCenterPos[obstaclesNum] =
+	double obstacleSize = 9.0;
+	double obstacleVelocity = 1.20;
+	Point* obstaclesCenterPos = (Point*)malloc(obstaclesNum * sizeof(Point));
+	RtlZeroMemory(obstaclesCenterPos, obstaclesNum * sizeof(Point));
+	for (int i = 0; i < obstaclesNum; i++) 
 	{
-		{0,0},
-		{0,0},
-		{0,0},
-		{0,0},
-		{0,0},
-		{0,0},
-		{0,0},
-		{0,0},
-		{0,0},
-		{0,0}
-	};
-	double obstaclesMovingDirection[obstaclesNum] =
+		double angle = cvtAngle2Rad((double)(rand() % 360));
+		double radius = ((double)rand() / RAND_MAX) * 2.0 * targetSafetyRadius;
+
+		obstaclesCenterPos[i].x = targetPos.x + radius * cos(angle);
+		obstaclesCenterPos[i].y = targetPos.y + radius * sin(angle);
+	}
+	double* obstaclesMovingDirection = (double*)malloc(obstaclesNum * sizeof(double));
+	RtlZeroMemory(obstaclesMovingDirection, obstaclesNum * sizeof(double));
+	for (int i = 0; i < obstaclesNum; i++) 
 	{
-		0,
-		0,
-		0,
-		0,
-		0,
-		0,
-		0,
-		0,
-		0,
-		0
-	};*/
-	
-	POBSTACLE obstacles[obstaclesNum] = { NULL };
+		obstaclesMovingDirection[i] = ((double)(rand() % 35800) + 100.0) / 100.0;
+	}
+	POBSTACLE* obstacles = (POBSTACLE*)malloc(obstaclesNum * sizeof(POBSTACLE));
+	RtlZeroMemory(obstacles, obstaclesNum * sizeof(POBSTACLE));
 	fors(
 		obstaclesNum,
 		initializeObstacles(&obstacles[j], obstaclesCenterPos[j], obstacleSize, obstacleVelocity, obstaclesMovingDirection[j]);
-		showObstacle(obstacles[j]);
 	);
-	buildCycleListByHuntersOriginalIndex(hunter); //补全了hunter结构的LIST_ENTRY项
 
 	size_t surroundingAttempCount = 0x0;
-	size_t patience = 800;
+	size_t patience = huntersNum * 20000;
+
+	ExFreeMemoryToNULL((PVOID*)&obstaclesMovingDirection);
+	ExFreeMemoryToNULL((PVOID*)&obstaclesCenterPos);
 
 	while (1)
 	{
 #ifdef _DEBUG
 		printf_cyan("******************************************************\n");
 #endif
-		if (!isSurroundingSuccess(hunter, target, obstacles)) // !isSurroundingSuccess(hunter, target, obstacles)
+		if (!isSurroundingSuccess(hunter, target)) // !isSurroundingSuccess(hunter, target)
 		{
-			printf("未收敛. surroundingAttempCount: %zu\n", surroundingAttempCount);
-			movingObstaclesRandomly(&obstacles);
+			movingObstaclesRandomly((POBSTACLE (*)[obstaclesNum])&obstacles);
 			fors(
 				huntersNum,
 				_SURROUNDING_changingHunterInfomationByRewardFunctionAndEnvironment(&hunter[j], target, obstacles);
 			);
-			fors(
-				huntersNum,
-				printf("[当前距离信息] %zu -> %lf\n", j, calculate_P$P_distance(hunter[j]->pos, target->pos));
-			);
+			if (surroundingAttempCount % 250 == 0)
+			{
+				printf("未收敛. surroundingAttempCount: %zu\n", surroundingAttempCount);
+				fors(
+					huntersNum,
+					printf("[当前距离/角度信息] %zu -> %lf\t%lf\n", j, calculate_P$P_distance(hunter[j]->pos, target->pos), calculate_P$P$P_unsignedAngle(hunter[j]->pos, target->pos, CONTAINING_RECORD(hunter[j]->hunterListEntry.Flink, HUSV, hunterListEntry)->pos) - PHASE_ANGLE);
+				);
+			}
+			movingNormalTargetRandomly(&target);
+			makePythonFile(fp, hunter, target, obstacles);
+#ifdef _DEBUG
 			for (size_t j = 0; j < huntersNum; j++)
 			{
 				size_t cnt = 0;
 				for (size_t i = 0; i < obstaclesNum; i++)
 				{
-					if (calculate_P$P_distance(hunter[j]->pos, obstacles[i]->center) <= 1.2 * obstacles[i]->size)
+					if (calculate_P$P_distance(hunter[j]->pos, obstacles[i]->center) <= 6.0 * obstacles[0]->size + 5.0 * Rh)
 					{
 						cnt++;
 					}
 				}
-				printf("[当前捕食者的视野内障碍数目] %zu -> %zu\n", j, cnt);
+				//printf("[当前捕食者的视野内障碍数目] %zu -> %zu\n", j, cnt);
 			}
-			movingNormalTargetRandomly(&target);
-#ifdef _DEBUG
 			fors(
 				obstaclesNum,
 				showObstacle(obstacles[j]);
@@ -154,10 +124,8 @@ int main()
 				}
 			);
 #endif
-			makePythonFile(fp, hunter, target, obstacles);
-			QAQ;
 			surroundingAttempCount++;
-			if (surroundingAttempCount == patience)
+			if (surroundingAttempCount % patience == 0)
 			{
 				system("pause");
 			}
@@ -169,12 +137,12 @@ int main()
 			printf("迭代次数: %zu\n", surroundingAttempCount);
 			fors(
 				huntersNum,
-				printf("[当前距离信息] %zu -> %lf\n", j, calculate_P$P_distance(hunter[j]->pos, target->pos));
+				printf("[当前距离/角度信息] %zu -> %lf\t%lf\n", j, calculate_P$P_distance(hunter[j]->pos, target->pos), calculate_P$P$P_unsignedAngle(hunter[j]->pos, target->pos, CONTAINING_RECORD(hunter[j]->hunterListEntry.Flink, HUSV, hunterListEntry)->pos) - PHASE_ANGLE);
 			);
+			makePythonFile(fp, hunter, target, obstacles);
 #ifdef _DEBUG
 			printf_yellow("\n最后一次显示的距离信息可能不符合条件<所有捕食者到目标的距离全都大于mostSafetyRadius + 1 * RtN + 1 * Rh>\n\n这是正常的，因为打印此消息时目标已经进行了一次移动！\n");
 #endif
-			makePythonFile(fp, hunter, target, obstacles);
 			QAQ;
 			break;
 		}
@@ -182,16 +150,14 @@ int main()
 	//流程正常结束，释放申请的内存，退出程序
 	fors(
 		obstaclesNum,
-		free(obstacles[j]);
-		obstacles[j] = NULL;
+		ExFreeMemoryToNULL((PVOID*)&obstacles[j]);
 	);
+	ExFreeMemoryToNULL((PVOID*)&obstacles);
 	fors(
 		huntersNum,
-		free(hunter[j]);
-		hunter[j] = NULL;
+		ExFreeMemoryToNULL((PVOID*)&hunter[j]);
 	);
-	free(target);
-	target = NULL;
+	ExFreeMemoryToNULL((PVOID*)&target);
 	fclose(fp);
 #ifdef _DEBUG
 	printf("Curr file name: %s\n", (const char*)__FILE__);
